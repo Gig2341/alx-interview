9601851b538f2b4dc7df57d31d61a3c7379b6b59
Gig2341 <danmata2341@gmail.com>
2024-02-08 11:54:30 +0000

    code requirement error fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,10 +8,13 @@ described in the grid.
 def island_perimeter(grid):
     ''' returns the perimeter of the island'''
     perimeter = 0
-    height = len(grid)
-    width = len(grid[0])
-    for i in range(height):
-        for j in range(width):
+    column = len(grid)
+    row = len(grid[0])
+    assert (1 <= row and column <= 100), "length must be between 1 an 100"
+    for i in range(column):
+        for j in range(row):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                "grid numbers must be 0 or 1"
             if (grid[i][j] == 1):
                 perimeter += 4
                 if grid[i][j-1] == 1:
